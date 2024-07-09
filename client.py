49617804e38ab73b98e577f4d8e39633f9de82c2
MC Vector 36 <constantinmihai804@gmail.com>
2024-07-09 15:44:42 +0300

    v1.2

--- client.py
+++ client.py
@@ -2,7 +2,6 @@ import argparse
 import threading
 from p2pnetwork.node import Node
 import stun
-import socket
 
 class PeerNode(Node):
     def __init__(self, host, port):
@@ -33,55 +32,26 @@ def start_node(host, port):
     node.start()
     return node
 
-def get_public_address(stun_server):
-    nat_type, external_ip, external_port = stun.get_ip_info(stun_host=stun_server, stun_port=3478)
+def get_public_address():
+    nat_type, external_ip, external_port = stun.get_ip_info(stun_host='stun.l.google.com', stun_port=19302)
     print(f"Public IP: {external_ip}, Public Port: {external_port}")
     return external_ip, external_port
-
-def register_with_bootstrap(bootstrap_host, bootstrap_port, public_ip, public_port):
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client.sendto("REGISTER".encode(), (bootstrap_host, bootstrap_port))
-    client.recvfrom(1024)  # ACK from server
-    client.sendto(f"{public_ip}:{public_port}".encode(), (bootstrap_host, bootstrap_port))
-
-    peers = []
-    while True:
-        data, _ = client.recvfrom(1024)
-        peer_info = data.decode().split(':')
-        peers.append((peer_info[0], int(peer_info[1])))
-        if len(peers) > 1:
-            break
-    return peers
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='P2P Network Node')
     parser.add_argument('--host', type=str, default="0.0.0.0", help='Host address to bind')
-    parser.add.argument('--port', type=int, default=5000, help='Port to bind')
-    parser.add.argument('--bootstrap_host', type=str, required=True, help='Bootstrap server host')
-    parser.add.argument('--bootstrap_port', type=int, required=True, help='Bootstrap server port')
-    parser.add.argument('--stun_server', type=str, required=True, help='STUN server host')
+    parser.add_argument('--port', type=int, default=5000, help='Port to bind')
 
     args = parser.parse_args()
 
-    public_ip, public_port = get_public_address(args.stun_server)
+    public_ip, public_port = get_public_address()
     print(f"Starting local node with public address {public_ip}:{public_port}")
-
-    # Register with the bootstrap server
-    peers = register_with_bootstrap(args.bootstrap_host, args.bootstrap_port, public_ip, public_port)
-    print(f"Peers found: {peers}")
 
     # Start the local peer node
     local_node = start_node(args.host, args.port)
 
-    # Connect to peers
-    for peer in peers:
-        local_node.connect_with_node(peer[0], peer[1])
-
     while True:
-        command = input("Enter command (send/exit): ")
-        if command == "send":
-            message = input("Enter message: ")
-            local_node.send_message(message)
-        elif command == "exit":
+        command = input("Enter command (exit to quit): ")
+        if command == "exit":
             local_node.stop()
             break
